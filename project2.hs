ba616397a4f04a03166fafe292d0ec20c1845f2a
Kevkev1 <kevin1226@live.nl>
2015-06-04 20:36:48 +0200

    Second

--- project2.hs
+++ project2.hs
@@ -23,29 +23,30 @@ parseHelp (x:xs) clauses
 	| isFact x = parseHelp xs (clauses ++ [Fact (getFactName x) True])
 	| otherwise = parseHelp xs (clauses ++ [Rule x (getVar x) rhs])
 	where
-		facts = getClauses (splitWords x) clauses
+		facts = map (convert clauses) (splitWords x)
 		rhs = getRHS facts clauses ("","")
 
-generateList :: Program -> String -> String -> [String]
-generateList prog str name
-	| facts == [] 	= genListHelp prog str (getFactsOf prog) (getRule prog name)
+generateList :: Program -> String -> [String]
+generateList prog query
+	| facts == [] 	= rmDup $ concat $ map (genListHelp prog (getVar query) (getFactsOf prog)) (getRules prog query)
 	| otherwise		= facts
 	where 
-		facts = getAllFacts prog name
+		facts = getAllFacts prog query
 
 genListHelp :: Program -> String -> [String] -> Clause -> [String]
-genListHelp prog var []	_		= [] 
-genListHelp prog var (x:xs) (Rule s v b)
-	| calcRule prog s (var, x) 	= x : genListHelp prog var xs (Rule s v b)
-	| otherwise 				= genListHelp prog var xs (Rule s v b)
-genListHelp prog var (x:xs) (Fact s b) = []
+genListHelp _ _ [] _ = []
+genListHelp prog var (f:facts) r
+	| calcRule prog [(convert2 r)] [(var,f)]	= f: genListHelp prog var facts r
+	| otherwise									= genListHelp prog var facts r
 
-getRule :: Program -> String -> Clause
-getRule [] str 					= Fact "Nope" False
-getRule ((Rule s v b):prog) str
-	| getLHS s "" == str 			= Rule s v b
-	| otherwise 				= getRule prog str		 
-getRule ((Fact s b):prog) str 	= getRule prog str
+rmDup = foldl (\seen x -> if x `elem` seen then seen else seen ++ [x]) []
+
+getRules :: Program -> String -> [Clause]
+getRules [] str 					= []
+getRules ((Rule s v b):prog) str
+	| getName s == getName str 	= (Rule s v b): getRules prog str
+	| otherwise 				= getRules prog str		 
+getRules ((Fact s b):prog) str 	= getRules prog str
 
 getFactsOf :: Program -> [String]
 getFactsOf [] 				 	= []
@@ -65,25 +66,29 @@ getAllFacts ((Fact s b):prog) str
 	| getName s == getName str 	= (getVar s): getAllFacts prog str
 	| otherwise					= getAllFacts prog str
 
-calcRule :: Program -> String -> (String, String) -> Bool
-calcRule []	_ _ = False
-calcRule prog x tup
-	= calcRuleHelper prog (subVar x tup) tup
-
-calcRuleHelper :: Program -> String -> (String,String) -> Bool
-calcRuleHelper prog str tup
-	| rhs == False 		= calcRuleHelper (removeFirst prog str) str tup
-	| otherwise			= True
+calcRule :: Program -> [String] -> [(String,String)] -> Bool
+calcRule _ [] _			= False
+calcRule _ _ []			= False
+calcRule prog (s:str) (t:tup)
+	| rhs == False 		= calcRule prog str tup
+	| s == ""			= False
+	| otherwise 		= True
 	where
-		facts = getClauses (splitWords str) prog
-		rhs = getRHS facts prog tup
+		facts = splitWords $ subVar s t
+		rhs = getRHS (map (convert prog) facts) prog t
 
-removeFirst :: Program -> String -> Program
-removeFirst [] str = []
-removeFirst ((Fact s b):prog) str = (Fact s b): removeFirst prog str
-removeFirst ((Rule s v b):prog) str
-	| getName s == getName str 	= prog
-	| otherwise 				= (Rule s v b): removeFirst prog str 
+convert :: Program -> String -> Clause
+convert [] x 	= Fact x False 
+convert ((Rule s v b):prog) x
+	| s == x 	= Rule s v b
+	| otherwise	= convert prog x 
+convert ((Fact s b):prog) x
+	| s == x 	= Fact s b
+	| otherwise = convert prog x
+
+convert2 ::  Clause -> String
+convert2 (Rule s v b) 	= s
+convert2 (Fact s b)		= s
 
 subVar :: String -> (String, String) -> String
 subVar [] tup = ""
@@ -96,12 +101,6 @@ subVarHelp var (sVar,cons)
 	| var == sVar 	= cons
 	| otherwise 	= var
 
-getPara :: String -> String
-getPara [] = ""
-getPara (x:xs) 
-	| x == ')' = (x:xs)
-	| otherwise = getPara xs
-
 isFact :: String -> Bool
 isFact [] = False
 isFact (x:xs)
@@ -109,16 +108,28 @@ isFact (x:xs)
 	| x == ':'  = False
 	| otherwise = isFact xs
 
+getPara :: String -> String
+getPara [] = ""
+getPara (x:xs) 
+	| x == ')' || x == ',' = (x:xs)
+	| otherwise = getPara xs
+
+getAllVar :: String -> [String]
+getAllVar [] = []
+getAllVar (x:xs) 
+	| x == '(' || x == ','	= (getVar (x:xs)): getAllVar (getPara xs)
+	| otherwise				= getAllVar xs
+
 getVar :: String -> String
 getVar [] =	""
 getVar (x:xs) 
-	| x == '(' = getVarHelp xs
+	| x == '(' || x == ',' = getVarHelp xs
 	| otherwise = getVar xs
 
 getVarHelp :: String -> String
 getVarHelp [] = ""
 getVarHelp (x:xs)
-	| x == ')' = ""
+	| x == ')' || x == ',' = ""
 	| otherwise = x : getVarHelp xs
 
 getRHS :: [Clause] -> Program -> (String, String) -> Bool
@@ -126,7 +137,7 @@ getRHS [] others _ = True
 getRHS ((Fact s b):xs) others tup
 	= b && elem (Fact s b) others && getRHS xs others tup
 getRHS ((Rule s v b):xs) others tup
-	| b == False	= calcRule others s tup
+	| b == False	= calcRule others [s] [tup]
 	| otherwise 	= elem (Rule s v b) others && getRHS xs others tup
 
 splitWords :: String -> [String]
@@ -139,81 +150,48 @@ split :: String -> String -> [String]
 split [] ys | ys == "" = []
 			| otherwise = [ys]
 split (x:xs) ys	
-	| isGoodName [x] 	= split xs (ys ++ [x])
+	| goodLetter 	= split xs (ys ++ [x])
 	| ys == ""		= split xs ""
 	| otherwise 	= ys : (split xs "")
-
-getClauses :: [String] -> Program -> [Clause]
-getClauses [] _ = []
-getClauses (x:xs) prog
-	= getFromProgram prog x : getClauses xs prog
-
-getFromProgram :: Program -> String -> Clause
-getFromProgram [] str = Fact str False
-getFromProgram ((Fact s b):xs) str
-	| s == str = Fact s b
-	| otherwise = getFromProgram xs str
-getFromProgram ((Rule s v b):xs) str
-	| getName s == getName str = Rule s v b
-	| otherwise = getFromProgram xs str
-
-getLHS :: String -> String -> String
-getLHS [] str = str
-getLHS (x:xs) str
-	| x == ':' || x == ' ' || x == '.' || x == '?' = str
-	| otherwise = getLHS xs (str ++ [x]) 
+	where
+		goodLetter = isLetter x || isNumber x || x == '(' || x == ')' || x == ','
 
 getFactName :: String -> String
 getFactName [] = ""
 getFactName (x:xs)
-	| isGoodName [x] = x : getFactName xs
+	| goodLetter = x : getFactName xs
 	| otherwise = ""
-	
-
-checkFromString :: Program -> String -> Bool
-checkFromString [] str = False
-checkFromString ((Fact s b):xs) str
-	| getLHS s "" == str = b
-	| otherwise = checkFromString xs str
-checkFromString ((Rule s v b):xs) str
-	| getLHS s "" == str = b
-	| otherwise = checkFromString xs str
-
-isGoodName :: String -> Bool
-isGoodName [] = True
-isGoodName (x:xs)
-	= (isLetter x || isNumber x || x == '(' || x == ')') && isGoodName xs 
+	where
+		goodLetter = isLetter x || isNumber x || x == '(' || x == ')' || x == ','
 
 evalOne :: String -> Program -> Either Bool [String]
 evalOne query prog
-	| checkIfVar var 					= Right $ generateList prog var query
-	| isRule prog query					= Left $ calcRule prog rule (getVar rule, var)
+	| checkIfVar var 					= Right $ generateList prog query
+	| isFact query == False				= Left $ calcRule prog rule varTuple
 	| otherwise							= Left $ checkFact prog query
 	where
 		var = getVar query
-		rule = getRuleFromString query prog
+		rule = getAllRules query prog
+		varTuple = createTuple rule var
+
+createTuple :: [String] -> String -> [(String, String)]
+createTuple (x:xs) str
+	= (getVar x, str): createTuple xs str
 
 checkFact :: Program -> String -> Bool
 checkFact [] str = False
 checkFact ((Rule s v b):prog) str = checkFact prog str
 checkFact ((Fact s b):prog) str
-	| getVar s == getVar str 	= b
+	| getVar s == getVar str && getName s == getName str 	= b
 	| otherwise 				= checkFact prog str
 
-isRule :: Program -> String -> Bool
-isRule [] str = False
-isRule ((Fact s b):prog) str = isRule prog str
-isRule ((Rule s v b):prog) str
-	| getName s == getName str 	= True
-	| otherwise 				= isRule prog str
-
-getRuleFromString :: String -> Program -> String
-getRuleFromString str [] 	= ""
-getRuleFromString str ((Fact s b):prog)
-							= getRuleFromString str prog 
-getRuleFromString str ((Rule s v b):prog)
-	| getName s == getName str 	= s
-	| otherwise 				= getRuleFromString str prog
+getAllRules :: String -> Program -> [String]
+getAllRules str [] 	= [""]
+getAllRules str ((Fact s b):prog)
+								= getAllRules str prog 
+getAllRules str ((Rule s v b):prog)
+	| getName s == getName str 	= s: getAllRules str prog
+	| otherwise 				= getAllRules str prog
 
 getName :: String -> String
 getName [] 				= ""
@@ -232,30 +210,46 @@ convert_to_list "" [] = []
 convert_to_list x [] = [x]
 convert_to_list x xs = x:xs
 
-putInList :: String -> [String] -> [String]
-putInList x list = list ++ [x]
-
 removeQ :: String -> String
 removeQ [] 		= ""
 removeQ (x:xs)
 	| x == '?' 	= ""
 	| otherwise = x: removeQ xs 
 
+beautify :: [String] -> String
+beautify [] = "\n"
+beautify (x:xs)
+	= "\t\t" ++  x ++ "\n" ++ beautify xs
+s = do
+	putStr "\n\t\tProgram:\n"
+	putStr $ beautify standard
+	getQuery standard
+
 start = do
 	putStrLn "Please input a line for the Program. (Send an empty line to signify the end)"
 	l <- getInput
 	putStrLn "Program:"
-	print l
+	putStr $ beautify l
 	getQuery l
 
 getQuery l = do
-	putStrLn "Please input a query to apply to the program"
+	putStr "Please input a query to apply to the program.\n" 
+	putStr "Type 'program' to see the program again.\n"
+	putStr "Use an empty line to close the program.\n"
 	line <- getLine
 	if null line
 		then return ()
 		else do
-			print $ evalOne (removeQ line) (parse l)
-			getQuery l
+			if line == "program"
+				then do
+					putStr "\n\t\tProgram:\n"
+					putStr $ beautify l
+					getQuery l
+				else do 
+					putStr "\t\tResult:\n\t\t "
+					putStrLn . either show show $ evalOne (removeQ line) (parse l)
+					putStr "\n"
+					getQuery l
 
 getInput = do
 	line <- getLine
@@ -263,19 +257,10 @@ getInput = do
         then return []
         else do  
         	if line == "standard"
-        	then return s1
+        	then return standard
         	else do
 				a <- getInput
 				return (convert_to_list line a)
 
-standard = ["p(a).", "p(b).", "p(c).", "q(a).", "q(b).", "s(X) :- p(X),q(X).", "r(X) :- s(X),q(X)."]
-s1 = ["q(a).","q(b).","p(a).","p(b).","p(c).","k(c).","k(d).","r(X):-p(X),q(Y),k(Y).", "t(X):-q(X)."]
-
-t1 = "a."
-t2 = "b."
-t3 = "c :- a,b."
-t4 = "d :- c,b."
-t5 = "e :- a,f."
-t6 = "g :- a,e."
-
-pTest = parse [t1, t2, t3, t4, t5, t6]
+standard = ["q(a).","q(b).","p(a).","p(b).","p(c).","k(c).","k(d).","r(X):-p(X),q(Y),k(Y).", "t(X):-q(X).", "t(X):-p(X)."]
+program = parse standard
